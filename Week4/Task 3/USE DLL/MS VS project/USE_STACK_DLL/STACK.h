69f9a8d019be49cb38ef2a5dcc4d4cd63463aa7c
BugrynYuriy <bugrynyuriy@gmail.com>
2012-03-08 00:16:49 +0200

    ololo

--- Week4/Task 3/USE DLL/MS VS project/USE_STACK_DLL/STACK.h
+++ Week4/Task 3/USE DLL/MS VS project/USE_STACK_DLL/STACK.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#ifndef _STACK_
+#define _STACK_
 
 struct STACK ; 
 
@@ -7,3 +9,18 @@ typedef STACK* (*STACKF)(int);
 typedef void (*DELETE_STACK)(STACK **stack);
 typedef void (*PUSH) (STACK ** stack,char value);
 typedef char (*POP)(STACK ** stack);
+
+STACKF createStack;
+DELETE_STACK deleteStack;
+DELETE_STACK clean;
+PUSH pushToStack;
+POP popToStack;
+POP getTop;
+POP isEmpty;
+
+char * getRandomArr(unsigned int size);
+bool initStackFromArr(unsigned int size ,char * arr,STACK **stack );
+bool initStackFromStack(STACK **stack , STACK **initstack);
+bool FillStackAndGetAllElements (int size = -1);
+
+#endif 
--- Week4/Task 3/USE DLL/MS VS project/USE_STACK_DLL/usedll.cpp
+++ Week4/Task 3/USE DLL/MS VS project/USE_STACK_DLL/usedll.cpp
@@ -2,63 +2,73 @@
 #include <windows.h> 
 #include <conio.h>
 #include "STACK.h"
+#include <time.h>
 
+
+
+bool initFunctions ( HINSTANCE *dll , LPCWSTR libary)
+{
+	*dll = LoadLibrary ( libary );
+
+	if (*dll ) 
+	{
+		(FARPROC&) createStack = GetProcAddress( *dll, LPCSTR("createStack") );
+		(FARPROC&) deleteStack = GetProcAddress( *dll, LPCSTR("deleteStack") );
+		(FARPROC&) clean = GetProcAddress( *dll, LPCSTR("clean") );
+		(FARPROC&) pushToStack = GetProcAddress( *dll, LPCSTR("push") );
+		(FARPROC&) popToStack = GetProcAddress( *dll, LPCSTR("pop") );
+		(FARPROC&) getTop = GetProcAddress( *dll, LPCSTR("getTop") );
+		(FARPROC&) isEmpty = GetProcAddress( *dll, LPCSTR("isEmpty"));
+
+		return 1;
+	}
+
+	else
+
+		return 0;
+}
 
 
 int main ()
 {
-	STACKF createStack;
-	DELETE_STACK deleteStack;
-	DELETE_STACK clean;
-	PUSH pushToStack;
-	POP popToStack;
-	POP getTop;
-	POP isEmpty;
 	
-	HINSTANCE mydll;
-	//mydll = LoadLibrary(TEXT("C:/Users/yura/Documents/Visual Studio 2010/Projects/STACK_DLL/Release/STACK_DLL")); 
-	mydll = LoadLibrary(TEXT("STACK_DLL"));
-	if (mydll ) 
+	HINSTANCE mydll; 
+	
+	if( initFunctions ( &mydll , TEXT ( "STACK_DLL" ) ) )
 	{
-		std::cout<<"Dynamic library:\n";
-		(FARPROC&)createStack = GetProcAddress(mydll, LPCSTR("createStack"));
-		(FARPROC&)deleteStack = GetProcAddress(mydll, LPCSTR("deleteStack"));
-		(FARPROC&)clean = GetProcAddress(mydll, LPCSTR("clean"));
-		(FARPROC&)pushToStack = GetProcAddress(mydll, LPCSTR("push"));
-		(FARPROC&)popToStack = GetProcAddress(mydll, LPCSTR("pop"));
-		(FARPROC&)getTop = GetProcAddress(mydll, LPCSTR("getTop"));
-		(FARPROC&)isEmpty = GetProcAddress(mydll, LPCSTR("isEmpty"));
+		
+		FillStackAndGetAllElements(-2);
 		
 		
-		
-		if(createStack)
+		if( createStack )
 		{
-			STACK *mystack=NULL;
+			STACK *mystack = NULL;
 			
 			char str [100];
-			int size,i=0;
+			int size , i=0;
 
-			std::cout<<"\nEnter string (Q - end): ";
-			while((str[i]=_getch())!='Q')
+			std::cout<< "\n\nEnter string (Q - end): ";
+			
+			while( ( str [i] = _getch() ) != 'Q')
 			{
 				std::cout<<str[i];
 				i++;
 			}
 			
 			size = i;
-			mystack = createStack(size);
+			mystack = createStack ( size ) ;
 			
-			for(int i=0 ;i<size;i++)
-				pushToStack(&mystack,str[i]);
+			for( int i=0 ; i < size ; i++ )
+				pushToStack ( &mystack , str[i] );
 
-			for(int i=0 ;i<size;i++)
-				str[i]=popToStack(&mystack);
+			for( int i=0 ; i < size ; i++ )
+				str[i] = popToStack ( &mystack );
 			
-			std::cout<<"\nRevers string : ";
-			str[size]='\0';
-			std::cout<<str;
+			std::cout<< "\nRevers string : ";
+			str[size] = '\0';
+			std::cout<< str;
 			
-			deleteStack(&mystack);				
+			deleteStack ( &mystack );				
 		}
 		
 		else
@@ -73,6 +83,114 @@ int main ()
 
 	
 	_getch();
-	FreeLibrary(mydll);
+	FreeLibrary ( mydll );
 	return 0;
+}
+
+char * getRandomArr(unsigned int size)
+{
+	char *initarr ;
+
+	try
+	{
+		initarr = new char [size];
+	}
+
+	catch(std::bad_alloc)
+	
+	{
+		return NULL;
+	}
+
+	for(unsigned int i = 0 ; i < size ; i++)
+		initarr[i] = rand ()%100;
+
+	return initarr;
+}
+
+bool initStackFromArr(unsigned int size ,char * arr,STACK **stack )
+{
+	for(unsigned int i = 0 ;i < size ; i++)
+	{
+		try
+		{
+			pushToStack(stack,arr[i]);
+		}
+
+		catch(...)
+		{
+			if(size != 0)
+				return false;
+			else
+				return true;
+		}
+	}
+
+	return true;
+}
+
+bool initStackFromStack(STACK **stack , STACK **initstack)
+{
+	
+	do
+	{
+		try
+		{
+			
+			pushToStack(stack, popToStack(initstack));
+		}
+
+		catch (...)
+		{
+			if(!isEmpty(initstack))
+				return false;
+			else 
+				return true;
+		}
+	}
+	while (!isEmpty(initstack));
+	return true;
+}
+
+bool FillStackAndGetAllElements (int size)
+{
+	srand ( time(NULL) ); // initialize random seed
+
+	if(size < 0)
+		size = rand() %100;
+	
+
+	char *initarr = getRandomArr(size);
+	
+	STACK *mystack = NULL , *mystackTest = NULL;
+
+	mystack = createStack(size);
+	mystackTest = createStack(size);
+
+	if( !(initStackFromArr((unsigned)size,initarr,&mystack) && initStackFromStack(&mystackTest,&mystack)) )
+	{
+		std::cout<<"\nInit stack test failed with size = "<<size;
+
+		delete [] initarr;
+		deleteStack(&mystack);
+		deleteStack(&mystackTest);
+		return false;
+	}
+	
+	std::cout<<"\nInit stack test passed with size = "<<size;
+	for(unsigned int i = 0; i<(unsigned)size; i++)
+		if( initarr[i] != popToStack(&mystackTest) )
+		{
+			std::cout<<"\nFill and get test failed with size = "<<size;
+			delete [] initarr;
+			deleteStack(&mystack);
+			deleteStack(&mystackTest);
+			return false;
+		}
+
+	std::cout<<"\nFill and get test passed with size = "<<size;
+	delete [] initarr;
+	deleteStack(&mystack);
+	deleteStack(&mystackTest);
+	return true;
 }
